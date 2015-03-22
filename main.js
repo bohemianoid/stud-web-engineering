2b56adaf58b2b28201d7fd8b9666e8a43a9b7f54
Álvaro Marco <malvaro@student.ethz.ch>
2015-03-22 22:17:54 +0100

    minimization partially implemented

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
-  // New menu Transparency
+  // New menu transparency
   var floatingMenu = $(".floating-menu");
-  floatingMenu.bind("mouseenter", function(){
+  floatingMenu.mouseenter(function(){
     $(this).fadeTo("fast", 1);
   });
-  floatingMenu.bind("mouseleave", function(){
+  floatingMenu.mouseleave(function(){
     $(this).fadeTo("fast", 0.2);
   });
+
+  // minimize
+  $(".minimize").dblclick(function() {
+    $(".floating-menu").toggleClass("floating-menu-not-minimized").toggleClass("floating-menu-minimized");
+  });
+
+  // pin
+  $(".pin").click(function() {
+    $(this).toggleClass("pin-on").toggleClass("pin-off");
+  });
+
+  // Draggable
+  // can jQuery UI be used?
 });
