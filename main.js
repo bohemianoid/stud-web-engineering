e86bec841f16c880c146a2840ac50c8afdfb1c02
Álvaro Marco <malvaro@student.ethz.ch>
2015-03-23 15:54:12 +0100

    minimization fully working + draggable + pin button functionality

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-  // New menu transparency
-  var floatingMenu = $(".floating-menu");
-  floatingMenu.mouseenter(function(){
-    $(this).fadeTo("fast", 1);
-  });
-  floatingMenu.mouseleave(function(){
-    $(this).fadeTo("fast", 0.2);
-  });
-
-  // minimize
+  // Minimize floating menu
   $(".minimize").dblclick(function() {
     $(".floating-menu").toggleClass("floating-menu-not-minimized").toggleClass("floating-menu-minimized");
+    $(".floating-menu .pin").toggleClass("opacity0");
+
+    if ($(".floating-menu").hasClass("floating-menu-not-minimized"))
+      $(".floating-menu li a").css({
+        "height":"8em",
+        "width":"8em"
+      });
+    else
+      $(".floating-menu li a").css({
+        "height":0,
+        "width":0
+      });
   });
 
   // pin
@@ -19,5 +22,12 @@ $(document).ready(function() {
   });
 
   // Draggable
-  // can jQuery UI be used?
+  $(".floating-menu").draggable();
+
+  // Move to nearest side
+  $(".floating-menu").mouseleave(function() {
+    if ($(".pin").hasClass("pin-on")) {
+      // TODO: write code here for nearest side
+    }
+  });
 });
