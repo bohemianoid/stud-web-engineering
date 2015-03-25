8794c4c7ea1f3c9cf86dc97bc3b7570eb7ea9bf6
rothsim <git@s.imon.ch>
2015-03-25 22:14:02 +0100

    Exercise 2.1 and 2.2

--- main.js
+++ main.js
@@ -1,33 +1,63 @@
 $(document).ready(function() {
   // Minimize floating menu
-  $(".minimize").dblclick(function() {
-    $(".floating-menu").toggleClass("floating-menu-not-minimized").toggleClass("floating-menu-minimized");
-    $(".floating-menu .pin").toggleClass("opacity0");
-
-    if ($(".floating-menu").hasClass("floating-menu-not-minimized"))
-      $(".floating-menu li a").css({
-        "height":"8em",
-        "width":"8em"
-      });
-    else
-      $(".floating-menu li a").css({
-        "height":0,
-        "width":0
-      });
+  $(".fancy-menu__minimize").dblclick(function() {
+    $(".fancy-menu").css({ width: "", height: "" }).toggleClass("fancy-menu--minimized");
   });
 
-  // pin
-  $(".pin").click(function() {
-    $(this).toggleClass("pin-on").toggleClass("pin-off");
+  // Pin
+  $(".fancy-menu__pin").click(function() {
+    $(".fancy-menu").toggleClass("fancy-menu--pinned");
   });
 
   // Draggable
-  $(".floating-menu").draggable();
+  $(".fancy-menu").draggable({ containment: "html" });
 
   // Move to nearest side
-  $(".floating-menu").mouseleave(function() {
-    if ($(".pin").hasClass("pin-on")) {
-      // TODO: write code here for nearest side
+  $(".fancy-menu").mouseleave(function() {
+    if (!$(".fancy-menu").hasClass("fancy-menu--pinned")) {
+      var viewportWidth = $(window).width();
+      var viewportHeight = $(window).height();
+      var menuWidth = $(this).outerWidth();
+      var menuHeight = $(this).outerHeight();
+      var menuOffset = $(this).offset();
+      var menuOffsetRight = viewportWidth - menuOffset.left - menuWidth;
+      var menuOffsetBottom = viewportHeight - menuOffset.top - menuHeight;
+
+      var distances = [
+        menuOffset.left,
+        menuOffset.top,
+        menuOffsetRight,
+        menuOffsetBottom
+      ]
+
+      var side = distances.indexOf(Math.min.apply(Math, distances));
+
+      switch (side) {
+        case 0:
+          $(this).css({ right: "", bottom: "" }).animate({ left: 0 }, 750);
+          break;
+        case 1:
+          $(this).css({ right: "", bottom: "" }).animate({ top: 0 }, 750);
+          break;
+        case 2:
+          $(this).css({ left: "", bottom: "", right: menuOffsetRight }).animate({ right: 0 }, 750);
+          break;
+        case 3:
+          $(this).css({ top: "", right: "", bottom: menuOffsetBottom }).animate({ bottom: 0 }, 750);
+          break;
+      }
+    }
+  });
+
+  // Stop on hover
+  $(".fancy-menu").mouseenter(function() {
+    $(this).stop();
+  });
+
+  // Responsive
+  $(window).resize(function(){
+    if ($(window).width() <= 768){
+      $(".fancy-menu").addClass("fancy-menu--minimized");
     }
   });
 });
