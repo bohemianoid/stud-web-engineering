cd1419bc3ffd4e533ed9e8336c9a13ce67327a43
rothsim <git@s.imon.ch>
2015-05-18 15:23:54 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+var express = require( 'express' );
+var app     = express();
+var http    = require( 'http' ).Server( app );
+var screens = [];
+var io      = require( 'socket.io' )( http );
 
-app.use(express.static('public'));
+app.use( express.static( 'public' ) );
 
-http.listen(8080, function(){
-    console.log('listening on *:8080');
-});
+io.on( 'connection', function( socket ) {
+  socket.on( 'screen', function( data ) {
+    if ( screens.indexOf( data.name ) == -1 ) {
+      screens.push( data.name );
+    }
+    socket.join( data.name );
+    console.log( data.name + ' connected' );
+
+    io.emit( 'remote', { screens: screens } );
+  } );
+
+  io.emit( 'remote', { screens: screens } );
+} );
+
+http.listen( 8080, function() {
+  console.log( 'listening on *:8080' );
+} );
--- public/script/remote.js
+++ public/script/remote.js
@@ -2,45 +2,59 @@ var currentImage = 0; // the currently selected image
 var imageCount = 7; // the maximum number of images available
 
 function showImage (index){
-    // Update selection on remote
-    currentImage = index;
-    var images = document.querySelectorAll("img");
-    document.querySelector("img.selected").classList.toggle("selected");
-    images[index].classList.toggle("selected");
+  // Update selection on remote
+  currentImage = index;
+  var images = document.querySelectorAll("img");
+  document.querySelector("img.selected").classList.toggle("selected");
+  images[index].classList.toggle("selected");
 
-    // Send the command to the screen
-    // TODO
-    alert("TODO send the index to the screen")
+  // Send the command to the screen
+  // TODO
+  alert("TODO send the index to the screen")
 }
 
 function initialiseGallery(){
-    var container = document.querySelector('#gallery');
-    var i, img;
-    for (i = 0; i < imageCount; i++) {
-        img = document.createElement("img");
-        img.src = "images/" +i +".jpg";
-        document.body.appendChild(img);
-        var handler = (function(index) {
-            return function() {
-                showImage(index);
-            }
-        })(i);
-        img.addEventListener("click",handler);
-    }
+  var container = document.querySelector('#gallery');
+  var i, img;
+  for (i = 0; i < imageCount; i++) {
+    img = document.createElement("img");
+    img.src = "images/" +i +".jpg";
+    document.body.appendChild(img);
+    var handler = (function(index) {
+      return function() {
+        showImage(index);
+      }
+    })(i);
+    img.addEventListener("click",handler);
+  }
 
-    document.querySelector("img").classList.toggle('selected');
+  document.querySelector("img").classList.toggle('selected');
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    initialiseGallery();
+  initialiseGallery();
 
-    document.querySelector('#toggleMenu').addEventListener("click", function(event){
-        var style = document.querySelector('#menu').style;
-        style.display = style.display == "none" || style.display == ""  ? "block" : "none";
-    });
-    connectToServer();
+  document.querySelector('#toggleMenu').addEventListener("click", function(event){
+    var style = document.querySelector('#menu').style;
+    style.display = style.display == "none" || style.display == ""  ? "block" : "none";
+  });
+
+  connectToServer();
 });
 
 function connectToServer(){
-    // TODO connect to the socket.io server
+  // TODO connect to the socket.io server
+  var socket = io();
+  socket.on( 'remote', function( data ) {
+    var settings = document.createElement( 'ul' );
+
+    for( var i = 0; i < data.screens.length; i++ ) {
+      var screen = document.createElement( 'li' );
+      screen.appendChild( document.createTextNode( data.screens[i] ) );
+      settings.appendChild( screen );
+    }
+
+    document.getElementById( 'menu' ).innerHTML = '';
+    document.getElementById( 'menu' ).appendChild( settings );
+  } );
 }
